469c4f8ec8b5075589e8b137b214ae9b8001f8b5
AleksandrDorofeev23 <110825244+AleksandrDorofeev23@users.noreply.github.com>
2023-07-19 02:29:29 +0300

    fix

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private long id;
     private String name;
     private String email;
